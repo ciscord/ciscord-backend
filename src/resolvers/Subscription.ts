eb67168ba783599bfcb4267cbccda73fa6a6c530
Michael Shants <shants.tech@gmail.com>
2023-03-12 14:54:34 -0500

    update subscription

--- src/resolvers/Subscription.ts
+++ src/resolvers/Subscription.ts
@@ -1,118 +1,129 @@
-import { objectType, stringArg } from 'nexus'
+import { subscriptionField, stringArg } from 'nexus'
 import { filter, pipe } from 'graphql-yoga'
 import ReactionSubscriptions from './Reaction/ReactionSubscriptions'
+import { pubsub } from '../context'
+export const newMessage = subscriptionField('newMessage', {
+  type: 'Message',
+  args: { channelUrl: stringArg(), tenant: stringArg() },
+  // subscribe: (_, { channelUrl, tenant }, context) => {
+  //   console.log(channelUrl, tenant, context, '===NEW_MESSAGE====')
+  //   return pubsub.subscribe('NEW_MESSAGE')
+  // },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    console.log(channelUrl, tenant, '===NEW_MESSAGE====')
+    return pipe(
+      pubsub.subscribe('NEW_MESSAGE'),
+      filter((payload) => payload.message.channel.url === channelUrl)
+    )
+  },
+  resolve: (payload) => {
+    console.log(payload, '=== NEW_MESSAGEpayload====')
+    return payload.message
+  }
+});
 
-export const Subscription = objectType({
-  name: 'Subscription',
-  definition(t) {
-    t.field('newMessage', {
-      type: 'Message',
-      args: { channelUrl: stringArg(), tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('NEW_MESSAGE'),
-          filter((payload) => payload.message.channel.url === channelUrl)
-        )
-      },
-      resolve: (payload) => {
-        return payload.message
-      }
-    })
+export const editMessage = subscriptionField('editMessage', {
+  type: 'Message',
+  args: { channelUrl: stringArg(), tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    return pipe(
+      pubsub.subscribe('EDITED_MESSAGE'),
+      filter((payload) => payload.editMessage.channel.url === channelUrl)
+    )
+  },
+  resolve: (payload) => payload
+})
 
-    t.field('editMessage', {
-      type: 'Message',
-      args: { channelUrl: stringArg(), tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('EDITED_MESSAGE'),
-          filter((payload) => payload.editMessage.channel.url === channelUrl)
-        )
-      }
-    })
+export const deleteMessage = subscriptionField('deleteMessage', {
+  type: 'Message',
+  args: { channelUrl: stringArg(), tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    return pipe(
+      context.pubsub.subscribe('DELETED_MESSAGE'),
+      filter((payload) => payload.deleteMessage.channel.url === channelUrl)
+    )
+  },
+  resolve: (payload) => payload
+})
 
-    t.field('deleteMessage', {
-      type: 'Message',
-      args: { channelUrl: stringArg(), tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('DELETED_MESSAGE'),
-          filter((payload) => payload.deleteMessage.channel.url === channelUrl)
-        )
-      }
-    })
+export const newNotification = subscriptionField('newNotification', {
+  type: 'Notification',
+  args: { receiverId: stringArg(), tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    return pipe(
+      context.pubsub.subscribe('NEW_NOTIFICATION'),
+      filter((payload) => payload.newNotification.channel.url === channelUrl)
+    )
+  },
+  resolve: (payload) => payload
+})
 
-    t.field('newNotification', {
-      type: 'Notification',
-      args: { receiverId: stringArg(), tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('NEW_NOTIFICATION'),
-          filter((payload) => payload.newNotification.channel.url === channelUrl)
-        )
-      }
-    })
+export const channelNewMessage = subscriptionField('channelNewMessage', {
+  type: 'Channel',
+  args: { communityUrl: stringArg(), tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    console.log(channelUrl, tenant, '===channelNewMessage====')
+    return pipe(
+      context.pubsub.subscribe('CHANNEL_NEW_MESSAGE'),
+      filter((payload) => payload.channelNewMessage.channel.url === channelUrl)
+    )
+  },
+  resolve: (payload) => payload
+})
 
-    t.field('channelNewMessage', {
-      type: 'Channel',
-      args: { communityUrl: stringArg(), tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        console.log(channelUrl, tenant, '===channelNewMessage====')
-        return pipe(
-          context.pubsub.asyncIterator('CHANNEL_NEW_MESSAGE'),
-          filter((payload) => payload.channelNewMessage.channel.url === channelUrl)
-        )
-      }
-    })
 
-    t.field('userWentOnline', {
-      type: 'User',
-      args: { tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('USER_WENT_ONLINE'),
-          filter((payload) => payload.tenant === tenant)
-        )
-      },
-      resolve: (payload) => {
-        return payload.user
-      }
-    })
-
-    t.field('userWentOffline', {
-      type: 'User',
-      args: { tenant: stringArg() },
-      subscribe: (_, { channelUrl, tenant }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('USER_WENT_OFFLINE'),
-          filter((payload) => payload.tenant === tenant)
-        )
-      },
-      resolve: (payload) => {
-        return payload.user
-      }
-    })
-
-    t.field('userTypingStatus', {
-      type: 'TypingStatus',
-      args: {
-        channelUrl: stringArg(),
-        tenant: stringArg(),
-        username: stringArg()
-      },
-
-      subscribe: ({ userTypingStatus }, { channelUrl, tenant, username }, context) => {
-        return pipe(
-          context.pubsub.asyncIterator('USER_TYPING_STATUS'),
-          filter(
-            (payload) =>
-              channelUrl === userTypingStatus.channelUrl &&
-              userTypingStatus.tenant === tenant &&
-              userTypingStatus.username !== username
-          )
-        )
-      }
-    })
-
-    ReactionSubscriptions(t)
+export const userWentOnline = subscriptionField('userWentOnline', {
+  type: 'User',
+  args: { tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    return pipe(
+      context.pubsub.subscribe('USER_WENT_ONLINE'),
+      filter((payload) => payload.tenant === tenant)
+    )
+  },
+  resolve: (payload) => {
+    return payload.user
   }
 })
+
+
+export const userWentOffline = subscriptionField('userWentOffline', {
+  type: 'User',
+  args: { tenant: stringArg() },
+  subscribe: (_, { channelUrl, tenant }, context) => {
+    return pipe(
+      context.pubsub.subscribe('USER_WENT_OFFLINE'),
+      filter((payload) => payload.tenant === tenant)
+    )
+  },
+  resolve: (payload) => {
+    return payload.user
+  }
+})
+
+
+export const userTypingStatus = subscriptionField('userTypingStatus', {
+  type: 'TypingStatus',
+  args: {
+    channelUrl: stringArg(),
+    tenant: stringArg(),
+    username: stringArg()
+  },
+
+  subscribe: ({ userTypingStatus }, { channelUrl, tenant, username }, context) => {
+    return pipe(
+      context.pubsub.subscribe('USER_TYPING_STATUS'),
+      filter(
+        (payload) =>
+          channelUrl === userTypingStatus.channelUrl &&
+          userTypingStatus.tenant === tenant &&
+          userTypingStatus.username !== username
+      )
+    )
+  },
+  resolve: (payload) => payload
+})
+
+    // ReactionSubscriptions(t)
+//   }
+// })
